e057df120eca1a85af1607b2a61a53d521292218
Dario <Dario@DESKTOP-VT261IB>
2022-04-21 17:27:50 +0200

    Aggiungere i file di progetto.

---
+++ ListaAppuntamenti/Appuntamento.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAppuntamenti
+{
+    internal class Appuntamento
+    {
+        internal string nomeDellAppuntamento;
+        internal string localitaAppuntamento;
+        internal DateTime dataEOraDellAppuntamento;
+
+
+        // costruttore
+
+        public Appuntamento(string nomeDellAppuntamento, string localitaAppuntamento, DateTime dataEOraDellAppuntamento)
+        {
+            this.nomeDellAppuntamento = nomeDellAppuntamento;
+            this.localitaAppuntamento = localitaAppuntamento;
+
+            if(dataEOraDellAppuntamento < DateTime.Now)
+            {
+                throw new InvalidDataException("La data dell'appuntamento non può essere precedente alla data di oggi");
+            }
+            this.dataEOraDellAppuntamento = dataEOraDellAppuntamento;
+        }
+
+        // Getters e Setters
+        public string GetNome(string nuovoNome)
+        {
+            return nuovoNome;
+        }
+        public string SetNome(string nuovoNome)
+        {
+            return nuovoNome;
+        }
+
+        public string GetLocalitaAppuntamento(string nuovaLocalitaAppuntamento)
+        {
+            return nuovaLocalitaAppuntamento;
+        }
+        public string SetLocalitaAppuntamento(string nuovaLocalitaAppuntamento)
+        {
+            return nuovaLocalitaAppuntamento;
+        }
+
+        public string GetDataEOra(string nuovaDataEOra)
+        {
+            return nuovaDataEOra;
+        }
+        public string SetDataEOra(string nuovaDataEOra)
+        {
+            return nuovaDataEOra;
+        }
+
+        public void CambioDataDellAppuntamento (DateTime nuovaDataAppuntamento)
+        {
+            if (nuovaDataAppuntamento < DateTime.Now)
+            {
+                throw new InvalidDataException("La data dell'appuntamento non può essere precedente alla data di oggi");
+            }
+            this.dataEOraDellAppuntamento = nuovaDataAppuntamento;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine();
+            Console.WriteLine("----- Appuntamento -------");
+            Console.WriteLine();
+            Console.WriteLine($"Tipo di appuntamento: {nomeDellAppuntamento}");
+            Console.WriteLine($"Luogo dell' appuntamento: {localitaAppuntamento}");
+            Console.WriteLine($"Data e Ora dell' appuntamento: {dataEOraDellAppuntamento}");
+            Console.WriteLine();
+            Console.WriteLine("------------------");
+        }
+    }
+}
---
+++ ListaAppuntamenti/Program.cs
@@ -0,0 +1,45 @@
+﻿using ListaAppuntamenti;
+
+List<Appuntamento> listaAppuntamenti = new List<Appuntamento>();
+
+Console.WriteLine("Vuoi aggiungere un appuntamento alla tua agenda? [Si/No]");
+string RispostaUtente = Console.ReadLine();
+
+if (RispostaUtente == "Si") 
+
+{
+    Console.WriteLine("Quanti appuntamenti vuoi aggiungere alla tua agenda?");
+    int numeroDiAppuntamentiAggiuntiDallUtente = int.Parse(Console.ReadLine());
+
+    for (int i = 0; i < numeroDiAppuntamentiAggiuntiDallUtente; i++)
+    {
+
+        Console.WriteLine();
+        Console.Write("Inserisci il nome del tuo appuntamento: ");
+        string nomeDellAppuntamento = Console.ReadLine();
+        Console.Write("Inserisci il luogo del tuo appuntamento: ");
+        string luogoDellAppuntamento = Console.ReadLine();
+
+        try
+        {
+            Console.Write("Inserisci la data del tuo appuntamento: ");
+            DateTime dataEOraDellAppuntamento = DateTime.Parse(Console.ReadLine());
+
+            Appuntamento nuovoAppuntamento = new Appuntamento(nomeDellAppuntamento, luogoDellAppuntamento, dataEOraDellAppuntamento);
+            listaAppuntamenti.Add(nuovoAppuntamento);
+        }
+        catch (InvalidDataException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    foreach (Appuntamento appuntamentoAggiuntoAllaLista in listaAppuntamenti)
+    {
+        appuntamentoAggiuntoAllaLista.ToString();
+    }
+
+} else if ( RispostaUtente == "No")
+           {
+
+           }
